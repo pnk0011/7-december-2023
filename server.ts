90da479afa9e938d2bc94529f2c31183c5de8faa
Pankaj Singh <pnk@Pankajs-MacBook-Air.local>
2023-11-09 02:00:37 +0530

    v2

--- server.ts
+++ server.ts
@@ -1,9 +1,7 @@
 import * as build from "@remix-run/dev/server-build";
-import { createRequestHandler } from "@netlify/remix-adapter";
+import { installGlobals } from "@remix-run/node";
+import { createRequestHandler } from "@remix-run/vercel";
 
-const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
+installGlobals();
 
-export default handler;
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
